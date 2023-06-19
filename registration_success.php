1136fa6b01b2e76146e5eb4433cc6770e038a093
lakeongit <122133828+lakeongit@users.noreply.github.com>
2023-06-19 14:47:09 -0400

    Create registration_success.php

---
+++ registration_success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration Success</title>
+</head>
+<body>
+  <h1>Registration Successful</h1>
+  <p>Congratulations! Your registration was successful.</p>
+  <p>Please check your email for further instructions to complete the registration process.</p>
+</body>
+</html>
